fc17193a76584be1c68d8f41b3ea8a9ab993e95d
Per Flitig <per.flitig@gmail.com>
2013-11-18 15:20:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# evil-corp-kata
+
+Evil Corp incremental code kata.
+
+## Getting Started
+Install the module with: `npm install evil-corp-kata`
+
+```javascript
+var evil_corp_kata = require('evil-corp-kata');
+evil_corp_kata.awesome(); // "awesome"
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2013 Per Flitig  
+Licensed under the MIT license.
---
+++ test/evil-corp-kata_test.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var evil_corp_kata = require('../lib/evil-corp-kata.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports['censorString'] = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'no input': function(test) {
+    test.expect(1);
+    test.equal(evil_corp_kata.censorString(), '', 'should return empty string.');
+    test.done();
+  },
+  'input with blacklisted word': function(test) {
+    test.expect(1);
+    test.equal(evil_corp_kata.censorString('You are a nice person'), 'You are a XXXX person', 'should censor word');
+    test.done();
+  },
+  'input with no blacklisted word': function(test) {
+    test.expect(1);
+    test.equal(evil_corp_kata.censorString('Hello!'), 'Hello!', 'should return same string');
+    test.done();
+  },
+  'input with multiple blacklisted words': function(test) {
+    test.expect(1);
+    test.equal(evil_corp_kata.censorString('Such a nice day with a bright sun, makes me happy'), 'Such a XXXX day with a bright XXX, makes me XXXXX', 'should censor each word');
+    test.done();
+  },
+};
