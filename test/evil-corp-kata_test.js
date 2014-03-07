a247bb79401f38f2f90cf3c3340b8fb1b7c173b9
Per Flitig <per.flitig@gmail.com>
2014-03-07 22:59:43 +0100

    Some minor refactorings, added coverage reports

--- test/evil-corp-kata_test.js
+++ test/evil-corp-kata_test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var evil_corp_kata = require('../lib/evil-corp-kata.js');
+var evil = require('../lib/evil-corp-kata.js');
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -29,32 +29,32 @@ exports['censorString'] = {
   },
   'no input': function(test) {
     test.expect(1);
-    test.equal(evil_corp_kata.censorString(), '', 'should return empty string.');
+    test.equal(evil.censorString(), '', 'should return empty string.');
     test.done();
   },
   'input with blacklisted word': function(test) {
     test.expect(1);
-    test.equal(evil_corp_kata.censorString('You are a nice person'), 'You are a XXXX person', 'should censor word');
+    test.equal(evil.censorString('You are a nice person'), 'You are a XXXX person', 'should censor word');
     test.done();
   },
   'input with no blacklisted word': function(test) {
     test.expect(1);
-    test.equal(evil_corp_kata.censorString('Hello!'), 'Hello!', 'should return same string');
+    test.equal(evil.censorString('Hello!'), 'Hello!', 'should return same string');
     test.done();
   },
   'input with multiple blacklisted words': function(test) {
     test.expect(1);
-    test.equal(evil_corp_kata.censorString('Such a nice day with a bright sun, makes me happy'), 'Such a XXXX day with a bright XXX, makes me XXXXX', 'should censor each word');
+    test.equal(evil.censorString('Such a nice day with a bright sun, makes me happy'), 'Such a XXXX day with a bright XXX, makes me XXXXX', 'should censor each word');
     test.done();
   },
   'input with conjugation of blacklisted word': function(test) {
     test.expect(1);
-    test.equal(evil_corp_kata.censorString('You are so friendly!'), 'You are so XXXXXXXXX', 'should censor whole word');
+    test.equal(evil.censorString('You are so friendly!'), 'You are so XXXXXXXXX', 'should censor whole word');
     test.done();
   },
   'input with unwanted words': function(test) {
     test.expect(1);
-    test.equal(evil_corp_kata.censorString('Objection is bad, a better thing to do, is to agree.'), 'Thoughtcrime is ungood, a gooder thing to do, is to crimestop.', 'should replace all unwanted words');
+    test.equal(evil.censorString('Objection is bad, a better thing to do, is to agree.'), 'Thoughtcrime is ungood, a gooder thing to do, is to crimestop.', 'should replace all unwanted words');
     test.done();
   },
 };
